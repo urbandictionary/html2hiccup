9e967a27fdbdcd56077fd249f7b5e99243d4222c
Aaron <apeckham@gmail.com>
2022-10-23 08:22:37 +0000

    refacgtoring

--- src/html2hiccup/core.clj
+++ src/html2hiccup/core.clj
@@ -9,53 +9,45 @@
 (def hiccup-vector-with-attrs? #(and (vector? %) (keyword? (first %)) (map? (second %))))
 (def keywordable? #(and (string? %) (re-matches #"[a-zA-Z][-a-zA-Z0-9:]+" %)))
 (def try-keyword #(if (keywordable? %) (keyword %) %))
+(def hiccup-walker #(fn [x] (if (hiccup-vector-with-attrs? x) (% x) x)))
 
 (defn remove-blank-strings-and-html-comments
-  [node]
-  (if (hiccup-vector-with-attrs? node)
-    (->> (concat [(first node) (second node)]
-                 (remove #(and (string? %) (or (str/blank? %) (re-matches #"<!-- .+ -->" %)))
-                         (rest (rest node))))
-         (into []))
-    node))
+  [x]
+  (->> (concat [(first x) (second x)]
+               (remove #(and (string? %) (or (str/blank? %) (re-matches #"<!-- .+ -->" %))) (rest (rest x))))
+       (into [])))
 
-(defn trim-all-strings [node] (if (string? node) (str/trim node) node))
+(defn trim-all-strings [x] (if (string? x) (str/trim x) x))
 
 (defn remove-empty-attr-maps
-  [node]
-  (if (and (hiccup-vector-with-attrs? node) (empty? (second node)))
-    (into [] (concat [(first node)] (rest (rest node))))
-    node))
+  [x]
+  (if (and (hiccup-vector-with-attrs? x) (empty? (second x)))
+    (into [] (concat [(first x)] (rest (rest x))))
+    x))
 
 (defn keywordize-attr-keys
-  [node]
-  (if (hiccup-vector-with-attrs? node)
-    (into []
-          (concat [(first node) (zipmap (keys (second node)) (map try-keyword (vals (second node))))]
-                  (rest (rest node))))
-    node))
+  [x]
+  (into []
+        (concat [(first x) (zipmap (keys (second x)) (map try-keyword (vals (second x))))] (rest (rest x)))))
 
 (defn attr-values-empty-strings->true
-  [node]
-  (if (hiccup-vector-with-attrs? node)
-    (into []
-          (concat [(first node)
-                   (zipmap (keys (second node)) (map #(if (= "" %) true %) (vals (second node))))]
-                  (rest (rest node))))
-    node))
+  [x]
+  (into []
+        (concat [(first x) (zipmap (keys (second x)) (map #(if (= "" %) true %) (vals (second x))))]
+                (rest (rest x)))))
 
-(defn fix-alpine-keywords [node] (if (and (keyword? node) (re-find #"^[:@]" (name node))) (name node) node))
+(defn fix-alpine-attrs [x] (if (and (keyword? x) (re-find #"^[:@]" (name x))) (name x) x))
 
 (defn html2hiccup
   [input]
   (->> input
        hickory/parse
        hickory/as-hiccup
-       (postwalk fix-alpine-keywords)
-       (postwalk remove-blank-strings-and-html-comments)
+       (postwalk fix-alpine-attrs)
+       (postwalk (hiccup-walker remove-blank-strings-and-html-comments))
        (postwalk trim-all-strings)
-       (postwalk attr-values-empty-strings->true)
-       (postwalk keywordize-attr-keys)
+       (postwalk (hiccup-walker attr-values-empty-strings->true))
+       (postwalk (hiccup-walker keywordize-attr-keys))
        (postwalk remove-empty-attr-maps)))
 
 (defn -main
